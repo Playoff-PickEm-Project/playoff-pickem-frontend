a1f9fb008b1701e673cc6a643a8cf24abdfa476b
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-12-20 02:58:46 -0500

    idk wtf is going on but something kinda works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Components/LoginPage/LoginPage'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [authorized, setAuthorized] = useState(false);
+
+  // {authenticated && <Route path="/authorizedContent" element={<AuthContent/>} />}
+
   return (
     <div className="App">
-      <h1>Playoff Pick'em League</h1>
-      <LoginPage/>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+
+      </Routes>
     </div>
   );
 }
--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,21 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Header = () => {
+const LoginPage = () => {
+  const [usernameValue, setUsername] = useState("");
+  const [passwordValue, setPassword] = useState("");
+
+  const handleLogin = () => {
+    async function login() {
+      const data = {
+        username: usernameValue,
+        password: passwordValue
+      }
+
+      try {
+        const response = await fetch("http://127.0.0.1:5000/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+          alert("login successful?");
+        }
+        else {
+          alert("failed login");
+        }
+      }
+      catch (error) {
+        console.log("something went wrong", error);
+      }
+    }
+
+    login();
+  };
+
+
   return (
     <div className="header">
+      <h1>Playoff Pick'em League</h1>
       <div className="text">Sign up</div>
+
       <div className="inputFields">
         <div className="input">
           <div className="text">username</div>
-          <input type="text" />
+          <input 
+            type="text"
+            id="username"
+            value={usernameValue}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+            />
         </div>
         <div className="input">
           <div className="text">password</div>
-          <input type="text" />
+          <input
+            type="password"
+            id="password"
+            value={passwordValue}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"/>
         </div>
+
+        <button onClick={handleLogin}>
+          attempt
+        </button>
       </div>
     </div>
   );
 };
 
-export default Header;
+export default LoginPage;
